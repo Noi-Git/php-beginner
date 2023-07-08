09df42014939dc67467370d803b73587d77b81fa
Noi-Git <noi.patsin@gmail.com>
2023-07-07 22:54:26 -0400

    var, const, and concat

--- data-type.php
+++ data-type.php
@@ -1,1 +1,23 @@
-"new page"
+<?php
+// Variables must be prefixed with $
+
+$name = 'Noi';
+$age = 40;
+// Use . for concatenation
+// Use "" the result is like string interporation ``
+echo 'My name is ' . $name;
+echo "</br>";
+
+echo "This is another way: $name";
+echo "</br>";
+
+echo "Another to concat variable: {$name}";
+echo "</br>";
+
+
+// Create constant
+define('HOST', 'localhost');
+define('DB_Name', 'dev_db');
+
+// will get localhost on the screen
+echo HOST;
